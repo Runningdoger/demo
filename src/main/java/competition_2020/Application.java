80b68e1523f6459cb04467577a119e751cdf5588
唐锦涛 <1>
2020-01-13 19:48:36 +0800

    1

--- src/main/java/competition_2020/Application.java
+++ src/main/java/competition_2020/Application.java
@@ -1,5 +1,6 @@
 package competition_2020;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@MapperScan( value = {"competition_2020.mapper"} )
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
